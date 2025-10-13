2e4074e7ebbfc9954d677e63077fef99f9bfa287
CactusInHawaii <cstud205403@vyatsu.ru>
2025-10-13 21:10:58 +0300

    добавил работы по алгаритмизации

---
+++ Daria Bychkova/Algorithmization/zd3-4/pas/1.pas
@@ -0,0 +1,8 @@
+﻿var r:integer;
+begin
+  for var x:=0 to 5 do
+  begin
+    r:=x*3-3*x*2+4*x-2;
+    writeln('x = ',x,' многочелен = ',r);
+  end;
+end.
---
+++ Daria Bychkova/Algorithmization/zd3-4/pas/3.pas
@@ -0,0 +1,19 @@
+﻿var r,c,wr,mr:integer;
+begin
+  Randomize;
+var w:= readinteger('введите кол-во девочек');
+for var i:=1 to w do
+  begin
+  wr:=wr+random(130,170);
+  end;
+
+var m:= readinteger('введите кол-во мальчиков');
+for var i:=1 to m do
+  begin
+  mr:=mr+random(150,190);
+  end;
+  
+writeln('средни рост девочек = ',(wr/w):0:2);
+writeln('средни рост мальчиков = ',(mr/m):0:2);
+
+end.
